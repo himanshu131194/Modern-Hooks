ff1005113cbd748abdab5027fcadf220a66a6c50
Himanshu Savita <himanshu131194@gmail.com>
2022-03-10 21:40:59 +0530

    use of useState

---
+++ src/components/Bookables/BookablesPage.js
@@ -0,0 +1,9 @@
+import BookablesList from "../Bookings/BookablesList";
+
+export default function BookablesPage(){
+    return(
+        <main className="bookables-page">
+            <BookablesList/>
+        </main>
+    )
+}
